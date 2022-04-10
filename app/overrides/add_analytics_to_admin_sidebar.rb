6600028ec7b017f2391099dea3e0ce745e303360
Yury Batenko <jurbat@gmail.com>
2022-04-10 19:17:53 +0700

    Fix menu override

--- app/overrides/add_analytics_to_admin_sidebar.rb
+++ app/overrides/add_analytics_to_admin_sidebar.rb
@@ -3,6 +3,7 @@ if Gem.loaded_specs['spree_core'].version >= Gem::Version.create('3.5.0')
     virtual_path: 'spree/admin/shared/sub_menu/_configuration',
     name: 'add_analytics_to_admin_sidebar',
     insert_bottom: '[data-hook="admin_configurations_sidebar_menu"]',
-    partial: 'spree/admin/shared/analytics_sidebar_menu'
+    partial: 'spree/admin/shared/analytics_sidebar_menu',
+    original: 'a172190dded672616531749986c69e42d96af7d0'
   )
 end
