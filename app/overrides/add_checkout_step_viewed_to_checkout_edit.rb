d614edb312ed064197214f524e39f8686a717b91
Hendra Nicholas <nicholash3ndra@gmail.com>
2022-09-10 11:00:04 +0700

    fixed deface origin source error

--- app/overrides/add_checkout_step_viewed_to_checkout_edit.rb
+++ app/overrides/add_checkout_step_viewed_to_checkout_edit.rb
@@ -5,7 +5,7 @@ unless spree_version >= Gem::Version.create('3.4.0') && spree_version < Gem::Ver
     name: 'add_segment_checkout_step_viewed_to_checkout_edit',
     insert_after: '[data-hook="checkout_content"]',
     partial: 'spree/shared/trackers/segment/checkout_step_viewed.js',
-    original: '806d2120a1ca1050fb093962fee10363c0944b0a'
+    original: '5fac7623f1db02ed867b682d64888989454661cc'
   )
 
   Deface::Override.new(
@@ -13,6 +13,6 @@ unless spree_version >= Gem::Version.create('3.4.0') && spree_version < Gem::Ver
     name: 'add_ga_checkout_step_viewed_to_checkout_edit',
     insert_after: '[data-hook="checkout_content"]',
     partial: 'spree/shared/trackers/google_analytics/checkout_step_viewed.js',
-    original: '0ac5c18a2db215bf8ba30470a0e5d69e00d0313a'
+    original: '5fac7623f1db02ed867b682d64888989454661cc'
   )
 end
